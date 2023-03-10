aa0482abf406f4d8aaba7f051ec8a4ce972cf407
RobertTracey <robertpatricktracey@gmail.com>
2023-03-10 15:26:35 +0000

    reinstall flask

--- app.py
+++ app.py
@@ -5,13 +5,9 @@ from flask import Flask, redirect, render_template, request, send_from_directory
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-import pymysql
-
 
 app = Flask(__name__, static_folder='static')
 csrf = CSRFProtect(app)
-
-
 
 # WEBSITE_HOSTNAME exists only in production environment
 if 'WEBSITE_HOSTNAME' not in os.environ:
@@ -35,6 +31,9 @@ db = SQLAlchemy(app)
 
 # Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
 migrate = Migrate(app, db)
+
+# The import must be done after db initialization due to circular import issue
+from models import names
 
 @app.route('/')
 def index():
